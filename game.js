23f53e0fe1dcbcbd742e71a4c98b58c030b1219b
Jrjh27CS <jrjh27cs@gmail.com>
2017-05-08 21:09:07 -0500

    Possible Fix

--- game.js
+++ game.js
@@ -112,3 +112,5 @@ window.onload = function () {
     function render() {
         game.debug.spriteInfo(player, 32, 32);
         game.debug.text('Score: ' + game.time.totalElapsedSeconds().toFixed(0)*1000, 64, 64);
+    }
+}
